17be927d33b60e4b3a70c9e6df2654d49410df63
brad-basilio <brad.basilioppy@gmail.com>
2025-07-18 10:03:27 -0500

    sin dark

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- Inline script to detect system dark mode preference and apply it immediately --}}
+        {{-- Force light mode by preventing dark class --}}
         <script>
-            (function() {
+            /*
+                        (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
 
                 if (appearance === 'system') {
@@ -17,17 +18,26 @@
                     }
                 }
             })();
+
+            */
+            
+            
+            // Force light mode - remove any dark class that might be applied
+            document.documentElement.classList.remove('dark');
         </script>
 
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
+        {{-- Inline style to set the HTML background color for light mode only --}}
         <style>
             html {
                 background-color: oklch(1 0 0);
             }
-
+            /*
+            
             html.dark {
                 background-color: oklch(0.145 0 0);
             }
+
+            */
         </style>
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
